47f1d1d742208833f8463d5693bf5cae31b69ae9
anuj <anuj@acadgild.com>
2017-05-15 12:04:37 +0530

    Android Sliding Tab Layout in Android

---
+++ AndroidSlidingTabLayoutExample/app/src/main/java/com/example/shriyanshu/androidslidingtablayoutexample/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.shriyanshu.androidslidingtablayoutexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declaring Your View and Variables
+
+    Toolbar toolbar;
+    ViewPager view_pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"Home","Events"};
+    int Numboftabs =2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        view_pager = (ViewPager) findViewById(R.id.pager);
+        view_pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(view_pager);
+
+
+
+    }
+
+
+
+}
---
+++ AndroidSlidingTabLayoutExample/app/src/main/java/com/example/shriyanshu/androidslidingtablayoutexample/Tab1Fragment.java
@@ -0,0 +1,18 @@
+package com.example.shriyanshu.androidslidingtablayoutexample;
+
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_1,container,false);
+        return v;
+    }
+}
