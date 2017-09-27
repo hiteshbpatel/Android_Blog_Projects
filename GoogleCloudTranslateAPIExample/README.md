58e97701bd80da1d6f3515c13d8061d6b98fafb3
anuj <anuj@acadgild.com>
2017-09-27 12:15:32 +0530

    Google Cloud Translate API Example Android

---
+++ GoogleCloudTranslateAPIExample/README.md
@@ -0,0 +1,6 @@
+# android-google-cloud-translate
+Example Android app using google-cloud-translate
+
+**NOTES:**
+- Before testing the app, replace `MainActivity.API_KEY` with your actual Translate API Key.
+- This is by no means a real world application. I strongly discourage you from saving your API_KEY in plain sight.
---
+++ GoogleCloudTranslateAPIExample/app/src/main/java/com/example/acadgild/translationdemo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.acadgild.translationdemo;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Handler;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
+public class MainActivity extends AppCompatActivity {
+
+    // todo API_KEY should not be stored in plain sight
+    private static final String API_KEY = "AIzaSyDbRCNSLzuwbVzwtO-0H5QSWfDnn8PGb60";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.text_view);
+        final Handler textViewHandler = new Handler();
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                TranslateOptions options = TranslateOptions.newBuilder()
+                        .setApiKey(API_KEY)
+                        .build();
+                Translate translate = options.getService();
+                final Translation translation =
+                        translate.translate("Hello World",
+                                Translate.TranslateOption.targetLanguage("de"));
+                textViewHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (textView != null) {
+                            textView.setText(translation.getTranslatedText());
+                        }
+                    }
+                });
+                return null;
+            }
+        }.execute();
+    }
+}
